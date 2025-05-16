f4d51eec89f9e3a10fbd0af4eb01ba6bc2955296
z6wdc <195cho@gmail.com>
2025-05-16 18:01:44 +0900

    refactor(entity): remove Type field to align with schema v1

--- internal/entity/notification.go
+++ internal/entity/notification.go
@@ -1,25 +1,7 @@
 package entity
 
-type NotificationType string
-
-const (
-    NotificationEmail NotificationType = "email"
-    NotificationPush  NotificationType = "push"
-    NotificationSMS   NotificationType = "sms"
-)
-
-func IsValidNotificationType(t NotificationType) bool {
-    switch t {
-    case NotificationEmail, NotificationPush, NotificationSMS:
-        return true
-    default:
-        return false
-    }
-}
-
 type Notification struct {
     ID      string
     UserID  int
     Message string
-    Type    NotificationType
 }
